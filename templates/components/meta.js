6a4c4a5edf19aa9739c16a7c28b188f10dda6d97
dhelafrank <ubongabasijerome8@gmail.com>
2024-04-10 14:15:40 +0100

    Init: Favicon

--- templates/components/meta.js
+++ templates/components/meta.js
@@ -1,5 +1,9 @@
 const meta = () => {
-    return `<meta name="theme-color" content="#000">`
+    return `
+    <meta name="theme-color" content="#000">
+    <link rel="shortcut icon" href="https://i.ibb.co/9p9zqZh/nacos-logo.png" />
+    
+    `
 }
 
 module.exports = { meta }
