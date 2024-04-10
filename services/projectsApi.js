06a11617ba36464a25cf71de130b03769f42ae31
dhelafrank <ubongabasijerome8@gmail.com>
2024-04-10 19:00:37 +0100

    Fix: ISE

--- services/projectsApi.js
+++ services/projectsApi.js
@@ -5,14 +5,14 @@ const apiUrl = process.env.API_SERVER_ROUTE
 
 const searchProjects = async (route, postData) => {
     try {
-        const response = await axios.post(`${apiUrl}${route}`, postData)
-        return response.data
+        const response = await axios.post(`${apiUrl}${route}`, postData);
+        return response.data;
     } catch (error) {
-        if (error.response.data) {
-            return error.response.data
+        if (error.response && error.response.data) {
+            return error.response.data;
         } 
-        return undefined
+        return undefined;
     }
-}
+};
 
 module.exports = { searchProjects }
