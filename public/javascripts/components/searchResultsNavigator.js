2116aaa3cc4d752305b8f0917ef1ee7abda1b599
dhelafrank <ubongabasijerome8@gmail.com>
2024-04-15 23:12:22 +0100

    Init: Error Page

--- public/javascripts/components/searchResultsNavigator.js
+++ public/javascripts/components/searchResultsNavigator.js
@@ -1,1 +1,8 @@
-console.log("I'm up");
+const searchResultCards = document.querySelectorAll(".search-results-card")
+searchResultCards.forEach(card =>{
+    card.addEventListener("click", (e)=>{
+        console.log(e.target)
+        console.log("routng...");
+        window.location.href = "/project"
+    })
+})
